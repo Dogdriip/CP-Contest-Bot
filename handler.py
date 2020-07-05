ff0a32f4349eb07892742757010761d389939965
Hyunseung Jeon <dogdriip@gmail.com>
2020-07-06 02:03:09 +0900

    [fix #4] Implement Atcoder

--- handler.py
+++ handler.py
@@ -3,8 +3,7 @@ from datetime import *
 import time
 import requests
 import os
-import codeforces
-# import atcoder
+import codeforces, atcoder
 
 KST = timezone(timedelta(hours=9))
 WEBHOOK_INFO_FILE = "webhook_info.json"
@@ -37,6 +36,7 @@ def lambda_func(event, context):
             }]
         })
         response_detail["codeforces_result"] = "failed"
+        response_detail["codeforces_body"] = ""
     else:
         # print(cf_list)
         for x in cf_list["contests"]:
@@ -82,13 +82,68 @@ def lambda_func(event, context):
                 ]
             })
         response_detail["codeforces_result"] = "succeed"
+        response_detail["codeforces_body"] = cf_list
     
+    """
+    Get Atcoder contests list.
+    """
+    at_list = atcoder.get_contests()
+    if not at_list['fetch']:
+        # if Atcoder fetch fails
+        attachments.append({
+            "color": "#FF0000",
+            "fields": [{
+                "value": "Error fetching Atcoder!"
+            }]
+        })
+        response_detail["atcoder_result"] = "failed"
+        response_detail["atcoder_body"] = ""
+    else:
+        # print(at_list)
+        for x in at_list["contests"]:
+            # Contest name.
+            name = x["name"]
+            # Contest starts.
+            starts = datetime.fromtimestamp(x["starts"], KST)
+            starts_s = starts.strftime("%Y-%m-%d %H:%M")
+            # Contest ends.
+            ends = datetime.fromtimestamp(x["ends"], KST)
+            ends_s = ends.strftime("%Y-%m-%d %H:%M")
+            # Contest URL.
+            url = x["url"]
 
+            # For attach text (Remaining time)
+            starts_delta = starts - now
+            attach_text = ""
+            if starts_delta.days != 0:
+                attach_text += f"{starts_delta.days}일 "
+            attach_text += f"{starts_delta.seconds // 3600}시간 후에 시작해요!"
 
-
-
-
-
+            attachments.append({
+                "mrkdwn_in": ["text"],
+                "color": "#9D3757",
+                "title": name,
+                "text": attach_text,
+                "fields": [
+                    {
+                        "title": "시작 일시",
+                        "value": starts_s,
+                        "short": True
+                    },
+                    {
+                        "title": "종료 일시",
+                        "value": ends_s,
+                        "short": True
+                    },
+                    {
+                        "title": "대회 URL",
+                        "value": url,
+                        "short": True
+                    }
+                ]
+            })
+        response_detail["atcoder_result"] = "succeed"
+        response_detail["atcoder_body"] = at_list
 
     """
     Send post to slack webhook, return response from lambda.
@@ -105,7 +160,7 @@ def lambda_func(event, context):
     )
     
     response = {}
-    if req.status_code != 200:
+    if req.status_code == 200:
         response = {
             "statusCode": 200,
             "body": json.dumps({
@@ -115,7 +170,7 @@ def lambda_func(event, context):
         }
     else:
         response = {
-            "statusCode": 500,
+            "statusCode": req.status_code,
             "body": json.dumps({
                 "message": "Slack Webhook failed!",
                 "detail": response_detail
