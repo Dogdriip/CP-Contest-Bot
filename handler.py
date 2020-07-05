7ec9dbdf2587cc475c9a3f56b86522c3745bcdb9
Hyunseung Jeon <dogdriip@gmail.com>
2020-07-06 01:05:10 +0900

    [fix #2] Configure handler.py, lambda_func

--- handler.py
+++ handler.py
@@ -1,24 +1,65 @@
 import json
+from datetime import *
+import time
+import requests
+import os
+# import codeforces, atcoder
+
+KST = timezone(timedelta(hours=9))
+WEBHOOK_INFO_FILE = "webhook_info.json"
+
+def lambda_func(event, context):
+    webhook_info = json.load(open(WEBHOOK_INFO_FILE, "r"))
+    webhook_url = webhook_info["INCOMING_WEBHOOK_URL"]
+
+    attachments = []  # for payloads. final result goes here
+
+    now = datetime.now(KST)
+    now_s = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+
+
+
+    """
+    Send post to slack webhook, return response from lambda.
+    """
+
+    """
+    payloads = {
+        'text': f'{now_s} 기준 콘테스트 목록',
+        'attachments': attachments
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    req = requests.post(
+        WEBHOOK_URL,
+        data=json.dumps(payloads),
+        headers={'Content-Type': 'application/json'}
+    )
+    """
+
+    # temp!
+
+    response = {}
+    if True:
+    # if req.status_code != 200:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Succeed",
+                "input": event
+            })
+        }
+    else:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "Failed",
+                "input": event
+            })
+        }
+
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
